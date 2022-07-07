e8e1a05e0788c362b4b4327e050b142ec6c7fb91
TManhNguyen <ntmanh2408@gmail.com>
2022-07-07 13:29:41 +0700

    add instruction for Shopify, webhook, script for webhook

---
+++ 3. webhook.site script.js
@@ -0,0 +1,93 @@
+theURL = "https://sgp1.blynk.cloud/external/api/update?token=pjFg4XOFm09VZtyVWbHbevDj1lFrCkyQ&"
+if (var('product_title') == "Orchestra MultiBag"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title') == "Sonata Tote"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title') == ("Jazz MiniPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title') == ("Jazz MiniPack - Pink")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title') == ("Ballad BackPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title') == ("Ballad BackPack - Blue")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+///////////////////////////////////////////////////////////////////////
+if (var('product_title2') == "Orchestra MultiBag"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title2') == "Sonata Tote"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+if (var('product_title2') == ("Jazz MiniPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title2') == ("Jazz MiniPack - Pink")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title2') == ("Ballad BackPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title2') == ("Ballad BackPack - Blue")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+///////////////////////////////////////////////////////////////////////////////////////////
+if (var('product_title3') == "Orchestra MultiBag"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title3') == "Sonata Tote"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+if (var('product_title3') == ("Jazz MiniPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title3') == ("Jazz MiniPack - Pink")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title3') == ("Ballad BackPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title3') == ("Ballad BackPack - Blue")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+/////////////////////////////////////////////////////////////////////////
+if (var('product_title4') == "Orchestra MultiBag"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title4') == "Sonata Tote"){
+    request(theURL + "v1=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+if (var('product_title4') == ("Jazz MiniPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title4') == ("Jazz MiniPack - Pink")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title4') == ("Ballad BackPack - Black")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
+
+if (var('product_title4') == ("Ballad BackPack - Blue")){
+    request(theURL + "v2=1", method='GET') // + var('product_quantity').to_string(), method='GET')
+}
